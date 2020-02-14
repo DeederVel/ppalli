8c26c4d7aae60fe254715c1ae1bda6b1a63448aa
Mattia Dui <mattioleddu@gmail.com>
2020-02-14 01:33:14 +0100

    Versioning

--- ppalli.php
+++ ppalli.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ppalli - Micro liveblogging plugin
  * Plugin URI: https://deedervel.com
  * Description: Micro liveblogging for Wordpress
- * Version:    2019.11.16
+ * Version:    2020.02.14
  * Author:     Mattia "DeederVel" Dui
  * Author URI:  https://deedervel.com
  * License:    CC BY-NC-SA 4.0
